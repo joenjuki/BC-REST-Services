81d16f73a25037481c7e057cda0365ede1fcd840
dvallejo <dvallejo@gettyimages.com>
2015-01-15 09:28:41 -0800

    Fix up directories

--- HelloWorld/Controllers/ContactsController.cs
+++ HelloWorld/Controllers/ContactsController.cs
@@ -13,17 +13,19 @@ namespace HelloWorld.Controllers
         private static int nextId = 100;
 
         public static List<Contact> contacts = new List<Contact>();
-        
+
         // GET: api/Contacts
         public IEnumerable<Contact> Get()
         {
             return contacts;
         }
+
         // GET: api/Contacts/5
         public Contact Get(int id)
         {
             return contacts.SingleOrDefault(t => t.Id == id);
         }
+
         // POST: api/Contacts
         public void Post([FromBody]Contact contact)
         {
@@ -33,10 +35,12 @@ namespace HelloWorld.Controllers
                 contacts.Add(contact);
             }
         }
+
         // PUT: api/Contacts/5
         public void Put(int id, [FromBody]Contact value)
         {
         }
+
         // DELETE: api/Contacts/5
         public void Delete(int id)
         {
