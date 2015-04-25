77f6b1dc3ae1a97d3829566fbca4606b4db6b41e
Dan <danv@cstructor.com>
2015-04-25 11:26:50 -0700

    use IIS instead of IIS Express
    add post data to client
    remove fiddler proxy from app.config

--- HelloWorldClient/Program.cs
+++ HelloWorldClient/Program.cs
@@ -1,17 +1,76 @@
 ﻿using System;
 using System.Net.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HelloWorldClient
 {
+    public class Contact
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("date-added")]
+        public DateTime DateAdded { get; set; }
+
+        [JsonProperty("phones")]
+        public Phone[] Phones { get; set; }
+    }
+
+    public class Phone
+    {
+        [JsonProperty("number")]
+        public string Number { get; set; }
+
+        [JsonProperty("phone-type")]
+        [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+        public PhoneType PhoneType { get; set; }
+    }
+
+    public enum PhoneType
+    {
+        Home,
+        Mobile,
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost.fiddler:49491/api/");
+
+            client.BaseAddress = new Uri("http://localhost/helloworldservice/api/");
+
+            var newContact = new Contact
+            {
+                Name = "New Name",
+                Phones = new[] { 
+                    new Phone { 
+                        Number = "425-111-2222",
+                        PhoneType = PhoneType.Mobile
+                    }
+                }
+            };
+
+            var newJson = JsonConvert.SerializeObject(newContact);
+            var postContent = new StringContent(newJson, System.Text.Encoding.UTF8, "application/json");
+            var postResult = client.PostAsync("contacts", postContent).Result;
+
+            Console.WriteLine(postResult.StatusCode);
+
+
             var result = client.GetAsync("contacts").Result;
             var json = result.Content.ReadAsStringAsync().Result;
             Console.WriteLine(json);
+
+            var obj = JsonConvert.DeserializeObject<List<Contact>>(json);
+
+            Console.WriteLine(obj[0].Id);
+
             Console.ReadLine();
         }
     }
